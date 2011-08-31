e5335234e5541c31d2c83b7f43b564b38a6a123f
Stefan Plantikow <stefan.plantikow@googlemail.com>
2011-08-31 02:38:12 +0200

    Microbenchmarking

--- src/test/scala/be/bolder/hoodie/linbin/test_linbin.scala
+++ src/test/scala/be/bolder/hoodie/linbin/test_linbin.scala
@@ -1,5 +1,11 @@
 package be.bolder.hoodie.linbin
 
+import compat.Platform
+import java.lang.IllegalStateException
+import util.Sorting
+import collection.mutable.ArrayOps
+import java.util.Arrays
+
 object SmokeTest {
   def main(args: Array[String]) {
     val data    = 0.to(30, 2).toArray
@@ -35,5 +41,44 @@ object SmokeTest {
     linbin1 += 9
 
     System.out.println("Check")
+
+
+    val times = 100000
+    val input = Array.fill[Double](4000000)(math.random)(Manifest.Double)
+    Sorting.quickSort(input)
+
+
+
+    {
+      System.out.println("plain binsearch")
+
+      val start  = Platform.currentTime
+      for (t <- 0.until(times)) {
+        val inputIndex = (math.random * input.length).toInt
+        val inputValue = input(inputIndex)
+        val result = input(Arrays.binarySearch(input, inputValue))
+        if (result != inputValue)
+          throw new IllegalStateException("Retrieval error")
+      }
+      val stop  = Platform.currentTime
+      System.out.println(stop-start)
+    }
+
+    {
+      System.out.println("empty linbin")
+
+      val linbin = LinBin.fromItemSortedArray[Double](22, 6, 11, input)
+      val start  = Platform.currentTime
+      for (t <- 0.until(times)) {
+        val inputIndex = (math.random * input.length).toInt
+        val inputValue = input(inputIndex)
+        val result = linbin( inputValue )
+        if (result != inputValue)
+          throw new IllegalStateException("Retrieval error")
+      }
+      val stop  = Platform.currentTime
+      System.out.println(stop-start)
+    }
+
   }
 }
